c1f8e84007bdc9a3f1583234c16e541a284835fa
gustavotassi <souzagustavotassi@gmail.com>
2022-05-07 10:45:02 -0300

    style: add smooth color transition

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -4,7 +4,7 @@ import { X } from 'phosphor-react';
 export function CloseButton() {
   return (
     <Popover.Button
-      className="top-5 right-6 absolute text-zinc-400 hover:text-zinc-100"
+      className="top-5 right-6 absolute text-zinc-400 hover:text-zinc-100 transition-colors"
       title="Fechar formulário de feedback"
     >
       <X weight="bold" className="w-4 h-4" />
