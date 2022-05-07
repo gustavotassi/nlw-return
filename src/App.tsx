8314a09cfcdf91df0c546555a6e4a95dc43cdf77
gustavotassi <souzagustavotassi@gmail.com>
2022-05-06 21:54:33 -0300

    refactor: remove surrounding div to only return widget component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { Widget } from './components/Widget';
 
 export function App() {
-  return (
-    <div className="bg-slate-800 h-screen w-screen">
-      <Widget />
-    </div>
-  );
+  return <Widget />;
 }
